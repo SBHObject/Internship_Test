28154b67a79a04b050eeb15ac20ef59a91a7e335
SBHObject <168382040+SBHObject@users.noreply.github.com>
2025-03-15 11:50:40 +0900

    [Fix] UnlockLevel 테이블 이름이 잘못된것 수정

---
+++ Internship_Test/Assets/01.Scripts/Manager/ItemDropManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropManager
+{
+    public List<ItemObject> DecideDropItem(List<int> dropTable)
+    {
+        for(int i = 0; i < dropTable.Count; i++)
+        {
+            
+        }
+
+        return null;
+    }
+}
